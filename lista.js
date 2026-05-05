81440070ba72161756aedf92e596143f9c68442a
Chablau03 <90479774+Chablau03@users.noreply.github.com>
2026-05-04 23:00:15 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -5,27 +5,31 @@ const bancoDeItens = [
         nome: "Peitoral do Eclipse Arcano",
         tipo: "equipamento",
         slot: "peito",
-        imagem: "https://i.imgur.com/xeugGBJ.png"
+        imagem: "https://i.imgur.com/xeugGBJ.png",
+        valor: 150.00
     },
     {
         id: "mago_luvas_01",
         nome: "Luvas do Eclipse Arcano",
         tipo: "equipamento",
         slot: "luvas",
-        imagem: "https://i.imgur.com/FfUQjK6.png"
+        imagem: "https://i.imgur.com/FfUQjK6.png",
+        valor: 60.50
     },
     {
         id: "mago_calca_01",
         nome: "Calça do Eclipse Arcano",
         tipo: "equipamento",
         slot: "pernas",
-        imagem: "https://i.imgur.com/LkBprw7.png"
+        imagem: "https://i.imgur.com/LkBprw7.png",
+        valor: 90.00
     },
     {
         id: "mago_gorro_01",
         nome: "Capuz do Eclipse Arcano",
         tipo: "equipamento",
         slot: "gorro",
-        imagem: "https://i.imgur.com/H7WZPUM.png"
+        imagem: "https://i.imgur.com/H7WZPUM.png",
+        valor: 75.20
     }
 ];

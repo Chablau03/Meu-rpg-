13a69f316d23b15cb75f14fd4ff3e963c3b9a4fb
Chablau03 <90479774+Chablau03@users.noreply.github.com>
2026-05-04 21:41:35 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,31 @@
+// Banco de dados dos itens
+const bancoDeItens = [
+    {
+        id: "mago_peito_01",
+        nome: "Peitoral do Eclipse Arcano",
+        tipo: "equipamento",
+        slot: "peito",
+        imagem: "https://i.imgur.com/xeugGBJ.png"
+    },
+    {
+        id: "mago_luvas_01",
+        nome: "Luvas do Eclipse Arcano",
+        tipo: "equipamento",
+        slot: "luvas",
+        imagem: "https://i.imgur.com/FfUQjK6.png"
+    },
+    {
+        id: "mago_calca_01",
+        nome: "Calça do Eclipse Arcano",
+        tipo: "equipamento",
+        slot: "pernas",
+        imagem: "https://i.imgur.com/LkBprw7.png"
+    },
+    {
+        id: "mago_gorro_01",
+        nome: "Capuz do Eclipse Arcano",
+        tipo: "equipamento",
+        slot: "gorro",
+        imagem: "https://i.imgur.com/H7WZPUM.png"
+    }
+];
